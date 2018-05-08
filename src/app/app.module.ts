7366fb4485523810b8f756d61c46275ebad2e797
Cameron Warje <cameronwarje@Camerons-MacBook-Pro-2.local>
2018-05-08 00:20:19 -0700

    Added message-list component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { MessageListComponent } from './components/message-list/message-list.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MessageListComponent
   ],
   imports: [
     BrowserModule
